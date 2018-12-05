a42ab43f48751874b8f743910ff56c666798cd9f
Mario Hidalgo García <MarioHidalgoTi@gmail.com>
2018-12-05 14:13:48 -0300

    Preparando proyecto para una arquitectura hexagonal

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-use MarioDevment\ApiUser\Kernel;
+use MarioDevment\ApiUser\Infrastructure\Symfony\Kernel;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
--- src/Infrastructure/Symfony/Kernel.php
+++ src/Infrastructure/Symfony/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace MarioDevment\ApiUser;
+namespace MarioDevment\ApiUser\Infrastructure\Symfony;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
