ed8d0334d5cfcd0cf4b2ca805e50e3b3f96b93b7
Mario Hidalgo García <MarioHidalgoTi@gmail.com>
2018-12-06 13:44:33 -0300

    Version para API

--- src/Context/Users/Infrastructure/RegisterController.php
+++ src/Context/Users/Infrastructure/RegisterController.php
@@ -5,14 +5,15 @@ namespace MarioDevment\ApiUser\Context\Users\Infrastructure;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 final class RegisterController extends AbstractController
 {
 
-	public function __invoke(Request $request, UserPasswordEncoderInterface $encoder)
+	public function __invoke(Request $request, UserPasswordEncoderInterface $encoder): Response
 	{
-		// TODO: Implement the best SOLID __invoke() method.
+		return $this->json('hello world');
 	}
 
 }
